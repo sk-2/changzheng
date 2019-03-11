d0f10f6d36dc223faa73e4853a17c9cae2abe6b4
sk-2 <2286729934@qq.com>
2019-03-11 11:06:59 +0800

    第一次的提交

---
+++ test2/src/day01/Wang.java
@@ -0,0 +1,115 @@
+package day01;
+
+import java.util.Scanner;
+
+public class Wang {
+
+	public static void main(String[] args) {
+	/*int a=5,b=10,c=18;
+	double d=6988.88,e=5999.99,f=4999.50;
+	System.out.println("--------------------商城库存清单--------------------");
+	System.out.print("  品牌型号	");
+	System.out.print("   	尺寸");
+	System.out.print("   	价格		");
+	System.out.print("      配置    ");
+	System.out.println("		库存数       ");
+	System.out.println("MacBookAir  "+"13.30  "+(d)+"  i5处理器4GB内存128GB固态硬盘   "+(a));
+	System.out.println("Thinkpad450  "+"14.00  "+(e)+"  i5处理器4GB内存500GB硬盘    "+(b));
+	System.out.println("ASUS-FL5800  "+"15.60  "+(f)+"  i7处理器4GB内存128GB固态硬盘    "+(c));
+	System.out.println("-------------------------------------------------------------------------------------------------------------");
+	System.out.println("总库存数："+(a+b+c));
+	System.out.println("库存商品总金额："+(double) (d*a+e*b+f*c));*/
+		
+	/*	String a="你爱我";
+		if(a.equals("你爱我")) {
+			System.out.println("给我好吃的");
+		}
+		else {
+			System.out.println("滚");
+		}*/
+		
+	/*	int a=5;
+		if(a%2==0) {
+			System.out.println("偶数");
+		}
+		else {
+			System.out.println("奇数");
+		}*/
+		
+//		int a=3,b=5,max=a>b?a:b;
+//		System.out.println(max);
+		
+	/*	int week;
+		//输入一个数据
+		Scanner s=new Scanner(System.in);
+		week=s.nextInt(); //nextLine
+		
+		switch(week) {
+		case 1:
+			System.out.println("工作日");
+			break;
+		case 2:
+			System.out.println("工作日");
+			break;
+		case 3:
+			System.out.println("工作日");
+			break;
+		case 4:
+			System.out.println("工作日");
+			break;
+		case 5:
+			System.out.println("工作日");
+			break;
+		case 6:
+			System.out.println("休息日");
+			break;
+		case 7:
+			System.out.println("休息日");
+			break;
+		}*/
+		
+		/*int x=0;
+		while(x<4) {
+			if(x==3) {
+				break;
+			}
+			x++;
+			System.out.println("x="+x);			
+		}*/
+		
+		/*int x=0;
+		do {
+			x++;
+			System.out.println("x="+x);
+		} while(x<4);*/
+	/*	int j=0;
+		for(int i=1;i<5;i++) {
+			j+=i;
+		}
+		System.out.println("和"+j);*/
+		
+	/*	for(int i=1;i<=9;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(" "+j+"*"+i+"="+(i*j));
+			}System.out.println("");
+		}*/
+		
+	/*	int a1=245,a2=570,a3=320;
+		int b1=2,b2=1,b3=1;
+		System.out.print("商品名称");
+		System.out.print("	单价	");
+		System.out.println("  数量");
+		System.out.println("T恤 	 "+a1+" 	 "+b1);
+		System.out.println("网球鞋 	 "+a2+" 	 "+b2);
+		System.out.println("网球拍 	 "+a3+" 	 "+b3);
+		System.out.println("实际消费金额："+(double)((a1*b1+a2*b2+a3*b3)*0.80));*/
+		
+	
+					
+				
+			
+		
+		
+		
+	}
+}
